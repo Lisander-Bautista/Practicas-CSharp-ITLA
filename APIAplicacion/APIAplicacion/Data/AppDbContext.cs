143132079df7c6e65b51f59b5e7ac98a96cd5bbd
Lisander-Bautista <lixanderinfante@gmail.com>
2026-05-29 23:57:12 -0400

    Primer commit - API proyecto

---
+++ APIAplicacion/APIAplicacion/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using APIAplicacion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIAplicacion.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
